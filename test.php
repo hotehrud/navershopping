16308d1fb894869a7d2a701edc195cb1d5ecbc6b
leejunghyun <mygumi@cizion.com>
2016-12-17 16:00:54 +0900

    파일 업로드 처리

--- test.php
+++ test.php
@@ -2,7 +2,16 @@
 
 $str = $_POST['dataString'];
 
-$fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/naver/test.txt","wb");
+if (file_exists("naver/navershopping.txt")) {
+    $dir = 'naver/';
+
+    rename($dir . "navershopping.txt", $dir . "before.txt");
+
+} else {
+    echo "not exist";
+}
+
+$fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/naver/navershopping.txt","wb");
 fwrite($fp,$str);
 fclose($fp);
 
